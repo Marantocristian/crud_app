88213ffbd175de17547e516ae46a228c942498ab
MarantoCristian <cristianronaldomarantopadilla@gmail.com>
2026-02-10 08:08:31 -0500

    so

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -14,7 +14,7 @@
                     @csrf
 
                     <div class="mb-3">
-                        <label for="name" class="form-label">Nombre *</label>
+                        <label for="name" class="form-label">Nombre</label>
                         <input type="text" class="form-control @error('name') is-invalid @enderror"
                                id="name" name="name" value="{{ old('name') }}" required autofocus>
                         @error('name')
@@ -23,7 +23,7 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email *</label>
+                        <label for="email" class="form-label">Email</label>
                         <input type="email" class="form-control @error('email') is-invalid @enderror"
                                id="email" name="email" value="{{ old('email') }}" required>
                         @error('email')
